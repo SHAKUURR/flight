da55f9e6326c2ea789771aa0e033f7a84c6895c4
iamshakur <salmanshakur7@gmail.com>
2023-10-01 18:27:30 +0100

    mobile view

--- index.js
+++ index.js
@@ -9,3 +9,15 @@ $(".trip-gallery").slick({
 	autoplay: true,
 	autoplaySpeed: 1500,
 });
+
+const header = document.querySelector(".nav-section");
+const elements = document.querySelector(".nav-list");
+const menu = document.querySelectorAll(".nav-link");
+const icon = document.querySelector("#menu-icon i");
+
+menu.forEach((element) => {
+	element.addEventListener("click", () => {
+		elements.classList.toggle("active");
+		icon.classList.toggle("active");
+	});
+});
